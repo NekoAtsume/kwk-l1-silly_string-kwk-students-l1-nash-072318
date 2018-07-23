064a22c74eaad0bff48431e70664404f448e3751
NekoAtsume <wolvesofgrace@yahoo.com>
2018-07-23 20:47:36 +0000

    automatically backed up by learn

--- sillystring.rb
+++ sillystring.rb
@@ -7,5 +7,5 @@ puts "I'M NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
 puts "wow + mom"
 
-puts "i really like programming".capitalize
+puts "i really like programming.".capitalize
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub('a','o').gsub('A','O')
